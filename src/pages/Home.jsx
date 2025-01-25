9ce4c85cff66274626f7dd70b85f5cfd8c263b0e
Hasankhan170 <hasankhanali0316@gmail.com>
2025-01-25 21:16:26 +0500

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -84,7 +84,7 @@ const MyComponent = () => {
   const allBlogs = async () => {
       try {
           const response = await axios.get("https://boiler-plate-mu.vercel.app/api/UserPost/post");
-          console.log("Fetched Posts:", response.data); 
+          console.log("Fetched Posts:", response.data.post); 
           setBlogs(response.data.posts); 
       } catch (err) {
           console.error("Error fetching posts:", err.message);
